d6331adef5a95b041ee727d12de781a4a09faeaa
rizzrr5 <134454835+rizzrr5@users.noreply.github.com>
2023-07-31 19:27:22 +0100

    MAde changes

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,11 @@ import logo from '../logo.png';
 import './App.css';
 
 class App extends Component {
+
+  captureFile =(event)=>{
+    event.preventDefault()
+    console.log('file captured')
+  }
   render() {
     return (
       <div>
@@ -18,7 +23,7 @@ class App extends Component {
         </nav>
         <div className="container-fluid mt-5">
           <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
+            <main role="main" className="col-lg-12 d-flex  justify-content-center ">
               <div className="content mr-auto ml-auto">
                 <a
                   href="http://www.dappuniversity.com/bootcamp"
@@ -27,18 +32,14 @@ class App extends Component {
                 >
                   <img src={logo} className="App-logo" alt="logo" />
                 </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
+             <h2>File Submission</h2>
+             <form>
+
+               <input type='file' onChange={this.captureFile} />
+               <input type='submit'/>
+             </form>
+              
+               
               </div>
             </main>
           </div>
--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css'
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { createRoot } from 'react-dom/client';
+const root = document.getElementById('root');
+//hello
+//asdas
+// Import createRoot from "react-dom/client" instead of directly from "react-dom"
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+// Use createRoot to render the App component
+createRoot(root).render(<App />);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA

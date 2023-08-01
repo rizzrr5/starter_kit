9fe9f20ffdc77ac7071c79a42db2ac64bb23c836
rizzrr5 <134454835+rizzrr5@users.noreply.github.com>
2023-08-01 16:17:45 +0100

    babelissue
    going to install babelcore7

---
+++ migrations/2_deploy_contract.js
@@ -0,0 +1,5 @@
+const Iprm = artifacts.require("Iprm");
+
+module.exports = function(deployer) {
+  deployer.deploy(Iprm);
+};
---
+++ test/Iprm.test.js
@@ -0,0 +1,15 @@
+const Iprm = artifacts.require("Iprm");
+
+require('chai')
+	.use(require('chai-as-promised'))
+	.should()
+
+// contract('Iprm',(accounts)=>{
+// 	let iprm
+
+// 	// describe('deployment',async()=>{
+// 	// 	iprm=await Iprm.deployed()
+// 	// 	const address=iprm.address
+// 	// 	console.log(address)
+// 	// })
+// })

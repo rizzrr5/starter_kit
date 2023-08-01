73e8f0da4bdfae0b2d4843efe95ad523898bc6d3
rizzrr5 <134454835+rizzrr5@users.noreply.github.com>
2023-08-01 17:28:10 +0100

    truffletest
    Fixed issue with babel,truffle test passed. Now moving on to deploying hash onto blockchain

--- test/Iprm.test.js
+++ test/Iprm.test.js
@@ -4,12 +4,33 @@ require('chai')
 	.use(require('chai-as-promised'))
 	.should()
 
-// contract('Iprm',(accounts)=>{
-// 	let iprm
+contract('Iprm',(accounts)=>{
+	let iprm
 
-// 	// describe('deployment',async()=>{
-// 	// 	iprm=await Iprm.deployed()
-// 	// 	const address=iprm.address
-// 	// 	console.log(address)
-// 	// })
-// })
+	before(async()=>{
+		iprm=await Iprm.deployed()
+	})
+
+	describe('deployment',async()=>{
+		it('deploys successfully',async()=>{
+		const address=iprm.address
+		assert.notEqual(address,0x0)
+		assert.notEqual(address,'')
+		assert.notEqual(address,undefined)
+		assert.notEqual(address,null)
+		// console.log(address)
+		})
+	
+	})
+
+	describe('storage',async()=>{
+		it('updates the storage',async()=>{
+			let hash
+			hash='abc123'
+			await iprm.set(hash)
+			const result=await iprm.get()
+			assert.equal(result,hash)
+			// console.log(result)
+		})
+	})
+})

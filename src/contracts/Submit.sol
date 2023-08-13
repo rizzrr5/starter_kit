559adfca79506357d27230132b543f13043bee87
rizzrr5 <134454835+rizzrr5@users.noreply.github.com>
2023-08-13 22:42:27 +0100

    Update Submit.sol
    some change

--- src/contracts/Submit.sol
+++ src/contracts/Submit.sol
@@ -1,1 +1,32 @@
-Submit.sol
+
+Contract Submit{
+
+	struct work{
+
+		  string ipfsHash;
+        address userAddress;
+        address moduleLeaderAddress;
+        uint256 timestamp;
+        string courseName;
+	} 
+  mapping(string => Submission) public submissions;
+
+    // Other contract code...
+
+    function submit(string memory _ipfsHash, string memory _courseName) public {
+    
+        submissions[_ipfsHash] = Submission({
+            ipfsHash: _ipfsHash,
+            userAddress: msg.sender,
+            moduleLeaderAddress: // Set module leader address here,
+            timestamp: block.timestamp,
+            courseName: _courseName
+        });
+
+        // Emit an event for the submission
+        emit DocumentSubmitted(msg.sender, _ipfsHash, _courseName, block.timestamp);
+    }
+
+    event DocumentSubmitted(address indexed sender, string ipfsHash, string courseName, uint256 timestamp);
+
+}
